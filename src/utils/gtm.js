6d48231798d8a4291dfa8d4d995b94dd3df086fa
AlfonsopechMag <alfonsoisup@gmail.com>
2024-08-21 11:13:18 -0500

    deploy ready

--- src/utils/gtm.js
+++ src/utils/gtm.js
@@ -1,6 +1,6 @@
 // gtm.js
 import TagManager from "react-gtm-module";
-const gtmId = "GTM--KRNXFCKQ"; // Replace with your GTM ID
+const gtmId = "GTM-KRNXFCKQ"; // Replace with your GTM ID
 
 export const initializeTagManager = () => {
   TagManager.initialize({ gtmId });
